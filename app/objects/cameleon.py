2ed6ed11d5ff2a2c8fd26f89655681431274a222
paulbnc <paul23buannic@gmail.com>
2025-11-21 09:48:16 +0100

    ojbiovb

--- app/objects/cameleon.py
+++ app/objects/cameleon.py
@@ -1,6 +1,6 @@
 import pygame
 
-class Cameleon:
+class Block:
     def __init__(self, 
                  w_width:int, 
                  w_height:int,
@@ -8,10 +8,15 @@ class Cameleon:
                  height:int = 30, 
                  width:int = 30, 
                  color:tuple = (20, 150, 50),
-                 speed:int = 10):
-        
-        self.x = float(w_width/2)
-        self.y = float(w_height + w_hud - height)
+                 speed:int = 10,
+                 x = None,
+                 y = None):
+        if (x is not None) and (y is not None):
+            self.x = x
+            self.y = y
+        else:
+            self.x = float(w_width/2)
+            self.y = float(w_height + w_hud - height)
         self.height = height
         self.width = width
         self.color = color
@@ -25,6 +30,16 @@ class Cameleon:
         self.w_h = w_height
         self.w_w = w_width
         self.w_hud = w_hud
+    
+    def check_collide(self):
+        if self.y > self.w_h + self.w_hud - self.height:
+            self.y = self.w_h + self.w_hud - self.height
+        if self.y < self.w_hud:
+            self.y = self.w_hud
+        if self.x > self.w_w - self.width:
+            self.x = self.w_w - self.width
+        if self.x < 0:
+            self.x = 0
 
     def handle_input(self)->None:
         keys = pygame.key.get_pressed()
@@ -41,14 +56,7 @@ class Cameleon:
             self.y += self.speed
             self.last_action = 'down'
         
-        if self.y > self.w_h + self.w_hud - self.height:
-            self.y = self.w_h + self.w_hud - self.height
-        if self.y < self.w_hud:
-            self.y = self.w_hud
-        if self.x > self.w_w - self.width:
-            self.x = self.w_w - self.width
-        if self.x < 0:
-            self.x = 0
+        self.check_collide()
 
         self.rect = pygame.Rect(self.x, 
                                 self.y, 
@@ -58,3 +66,54 @@ class Cameleon:
     def draw(self, screen)->None:
         pygame.draw.rect(screen, self.color, self.rect)
         #pour un skin en png : screen.blit(self.skin, (self.x, self.y))
+    
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+        self.rect = pygame.Rect(self.x, 
+                                self.y, 
+                                self.width, 
+                                self.height)
+
+class Cameleon:
+    def __init__(self,
+                 w_width:int, 
+                 w_height:int,
+                 w_hud:int, 
+                 n_blocks:int = 4,
+                 height:int = 30, 
+                 width:int = 30, 
+                 color:tuple = (20, 150, 50),
+                 speed:int = 5):
+        self.blocks = []
+
+        x = float(w_width/2)
+        y = float((w_height + w_hud - height)/2)
+
+        for i in range(n_blocks):
+            block = Block(w_width, 
+                                w_height,
+                                w_hud, 
+                                height, 
+                                width, 
+                                color,
+                                speed,
+                                x,
+                                y)
+            y += block.height
+            self.blocks.append(block)
+  
+    def draw(self, screen):
+        for block in self.blocks:
+            block.draw(screen)
+
+    def handle_input(self):
+        head = self.blocks[0]
+        old_positions = [(head.x, head.y)]
+        head.handle_input()
+
+        for i in range(1, len(self.blocks)):
+            block = self.blocks[i]
+            prev = self.blocks[i-1]
+            old_positions.append((block.x, block.y))
+            block.move(old_positions[i-1][0], old_positions[i-1][1])
